1f9376054e0dd132430e1572055d9d3b0305e15c
Justinas1414 <sileika.justinas@yahoo.com>
2024-03-20 20:37:24 +0200

    Add Driver class

---
+++ src/test/java/lt/justinas/pom/utils/Driver.java
@@ -0,0 +1,32 @@
+package lt.justinas.pom.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver {
+    private static ThreadLocal<WebDriver> chromeDriver = new ThreadLocal<>();
+
+    public static void setUpChrome() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.85");
+        //options.addArguments("window-size=5000,3000");
+
+        chromeDriver.set(new ChromeDriver(options));
+
+    }
+
+    public static WebDriver getChromeDriver() {
+        return chromeDriver.get();
+    }
+
+    public static void quitChromeDriver() {
+
+        chromeDriver.get().quit();
+        chromeDriver.remove();
+    }
+}
