1585e8c62a95504c31405381e165ba26d437e50a
Justinas1414 <sileika.justinas@yahoo.com>
2024-03-25 20:10:37 +0200

    Add TestBase class updated

--- src/test/java/lt/justinas/pom/tests/TestBase.java
+++ src/test/java/lt/justinas/pom/tests/TestBase.java
@@ -1,15 +1,18 @@
 package lt.justinas.pom.tests;
 
 import lt.justinas.pom.pages.Common;
+import lt.justinas.pom.utils.TestListner;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListner.class)
 public abstract class TestBase {
     @BeforeMethod
-    public abstract void setUp ();
+    public abstract void setUp();
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         Common.quitChromeDriver();
     }
 }
